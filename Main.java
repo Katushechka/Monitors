09c43c48c18e8d5d61edaf4af5982ca3523955d6
Katushechka <ekaterina.korotetskaya@gmail.com>
2021-10-23 19:03:47 +0200

    Små ändringar

--- Main.java
+++ Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main (String [] args)
-    {
+    public static void main (String [] args){
         MonitorGUI gui = new MonitorGUI();
         gui.Start();
 
